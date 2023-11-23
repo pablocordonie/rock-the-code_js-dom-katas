8ff612c39424d95d775af4eccacec2230672bc6e
pablocordonie <pablogarcia.93.06@gmail.com>
2023-11-23 13:22:14 +0100

    feat: propose the following exercises to be solved

---
+++ ejercicio-7/index.js
@@ -0,0 +1,5 @@
+/*
+Utiliza el array para crear dinamicamente una lista ul > li de elementos en el div de html con el atributo data-function="printHere".
+
+const places = ["Gondor", "Mordor", "Rivendel", "La Comarca", "Nümenor"];
+*/
